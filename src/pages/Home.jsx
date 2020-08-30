6062e8a2696f7bdca1d630f340ec7e7e33930cdd
talgat55 <lightx_design@yahoo.com>
2020-08-30 22:04:16 +0600

    add show post in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import {useQuery} from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import {Grid, GridColumn, GridRow} from "semantic-ui-react";
+import PostCard from "../components/PostCard";
 
 
-const Home = () =>(
-  <>
-      Home
-  </>
-);
+const Home = () => {
+    const {loading, data: {getPosts: posts}} = useQuery(FETCH_POST_QUERY);
 
-export default  Home;
+    return (
+        <Grid columns={3}>
+            <GridRow>
+                <h1>Posts</h1>
+
+            </GridRow>
+            <GridRow>
+                {
+                    loading ?
+                        (<p>Loading posts</p>)
+                    :
+                        posts && posts.map(item=>(
+                            <GridColumn key={item.id}>
+                                <PostCard item={item} />
+                            </GridColumn>
+                        ))
+                }
+            </GridRow>
+        </Grid>
+    )
+};
+const FETCH_POST_QUERY = gql`
+    {
+        getPosts{
+            id body createdAt username likeCount
+            likes{
+                username
+            }
+            commentCount
+            comments{
+                id username createdAt body
+            }
+        }
+    }
+`;
+export default Home;
