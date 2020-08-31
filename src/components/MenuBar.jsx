b50e500c84fc758561b52fa7f3c40a0e7107cebb
talgat55 <lightx_design@yahoo.com>
2020-08-31 18:25:49 +0600

    add logout

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {Menu, Segment} from 'semantic-ui-react';
 import {Link}  from 'react-router-dom';
+import {AuthContext} from "../context/auth";
 
 const MenuBar = () => {
-
+    const {user, logout} = useContext(AuthContext);
     const pathName = window.location.pathname;
     const path = pathName === '/' ? 'home' : pathName.substr(1);
     const [activeItem, setActiveItem] = useState(path);
@@ -11,47 +12,73 @@ const MenuBar = () => {
         setActiveItem(name);
     }
 
-    return (
-        <>
-            <Menu pointing secondary>
+    const menuBar = user ?
+        (
+            <>
+                <Menu pointing secondary>
 
-                <Menu.Item
-                    name="home"
-                    active={activeItem === 'home'}
-                    onClick={handleClick}
-                    as={Link}
-                    to="/"
-                />
-
-                <Menu.Item
-                    name="login"
-                    active={activeItem === 'login'}
-                    onClick={handleClick}
-                    as={Link}
-                    to="/login"
-                />
-
-                <Menu.Item
-                    name="register"
-                    active={activeItem === 'register'}
-                    onClick={handleClick}
-                    as={Link}
-                    to="/register"
-                />
-
-                <Menu.Menu  position="right">
                     <Menu.Item
-                        name="logout"
-                        active={activeItem === 'logout'}
+                        name={user.username}
+                        active
                         onClick={handleClick}
                         as={Link}
-                        to="/logout"
+                        to="/"
                     />
-                </Menu.Menu>
+                    <Menu.Menu  position="right">
+                        <Menu.Item
+                            name="logout"
+                            onClick={logout}
+                            as={Link}
+                            to="/logout"
+                        />
+                    </Menu.Menu>
 
-            </Menu>
-        </>
-    )
+                </Menu>
+            </>
+        ) :
+        (
+            <>
+                <Menu pointing secondary>
+
+                    <Menu.Item
+                        name="home"
+                        active={activeItem === 'home'}
+                        onClick={handleClick}
+                        as={Link}
+                        to="/"
+                    />
+
+                    <Menu.Item
+                        name="login"
+                        active={activeItem === 'login'}
+                        onClick={handleClick}
+                        as={Link}
+                        to="/login"
+                    />
+
+                    <Menu.Item
+                        name="register"
+                        active={activeItem === 'register'}
+                        onClick={handleClick}
+                        as={Link}
+                        to="/register"
+                    />
+
+                    <Menu.Menu  position="right">
+                        <Menu.Item
+                            name="logout"
+                            active={activeItem === 'logout'}
+                            onClick={handleClick}
+                            as={Link}
+                            to="/logout"
+                        />
+                    </Menu.Menu>
+
+                </Menu>
+            </>
+        );
+
+    return menuBar;
 }
 
 export default MenuBar;
